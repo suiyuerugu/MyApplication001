4184c121b25f2b79abbcef3b2847faea1d2e9a48
weixijie <3337183289@qq.com>
2017-10-04 18:17:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wxj/myapplication001/MainActivity01.java
@@ -0,0 +1,13 @@
+package com.example.wxj.myapplication001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.framlayout);
+    }
+}
