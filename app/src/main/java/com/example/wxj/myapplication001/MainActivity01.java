be7e40a3ae4b06a5efaea7f52ea3e5f6cd6ec8bd
weixijie <3337183289@qq.com>
2017-10-04 19:53:55 +0800

    第一次修改

--- app/src/main/java/com/example/wxj/myapplication001/MainActivity01.java
+++ app/src/main/java/com/example/wxj/myapplication001/MainActivity01.java
@@ -11,7 +11,7 @@ public class MainActivity01 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.framlayout);
+        setContentView(R.layout.tablelayout);
 
 
     }
