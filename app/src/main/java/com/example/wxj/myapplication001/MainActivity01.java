205f88d97b537a9c1660baa8aeec86bfe780bced
weixijie <3337183289@qq.com>
2017-10-04 19:29:46 +0800

    第一次修改

--- app/src/main/java/com/example/wxj/myapplication001/MainActivity01.java
+++ app/src/main/java/com/example/wxj/myapplication001/MainActivity01.java
@@ -2,6 +2,9 @@ package com.example.wxj.myapplication001;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity01 extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity01 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.framlayout);
+        LinearLayout ll=new LinearLayout(this);
+        LinearLayout.LayoutParams lp=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        TextView tv=new TextView(this);
+        tv.setText("16软2");
+
+        ll.addView(tv,lp);
+
+        setContentView(ll);
     }
 }
